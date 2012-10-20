40e029634632cc228c4e729f529373bb97854496
Southernbear <xq5530303@gmail.com>
2012-10-20 18:25:38 +0800

    implement heap sort

---
+++ src/HeapSort.java
@@ -0,0 +1,58 @@
+import java.util.Comparator;
+
+/**
+ *  Heap sort algorithm implementation and the algorithm is unstable.
+ *  This implementation has side effect on array.
+ * 
+ * @author	Southernbear
+ */
+
+public class HeapSort implements SortAlgorithm{
+	
+	/**
+	 * @param	array	array to be sorted, may be modified after call.
+	 * @param	comparator	decides the order in array.
+	 * @return	reference to {@link array}
+	 */
+	public <T> T[] sort(T[] array, Comparator<T> comparator) {
+		int len = array.length;
+	
+		/* Build maximum heap */
+		for(int i = len/2; i >= 0; i--)
+			maxHeapify(array, len, comparator, i);
+
+		for(int i = len-1; i > 0; i--){
+			T temp = array[0];
+			array[0] = array[i];
+			array[i] = temp;
+			
+			len--;
+			maxHeapify(array, len, comparator, 0);
+		}
+		
+		return array;
+	}
+	
+	private <T> void maxHeapify(T[] array, int len, Comparator<T> comparator, int parent){
+		int leftChild = parent;
+		int rightChild = parent;
+		int max = parent;
+		
+		/* Find maximum among parent, left child and right child */
+		if(leftChild < len && comparator.compare(array[leftChild], array[max]) > 0){
+			max = leftChild;
+		}
+		if(rightChild < len && comparator.compare(array[rightChild], array[max]) > 0){
+			max = rightChild;
+		}
+		
+		/* Swap data in parent and max and then propagate */
+		if(parent != max){
+			T temp = array[parent];
+			array[parent] = array[max];
+			array[max] = temp;
+			
+			maxHeapify(array, len, comparator, max);
+		}
+	}
+}
