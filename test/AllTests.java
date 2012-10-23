60882c108cbefa075b8867d36796bd65503dfef5
CFWei <s124210@gmail.com>
2012-10-23 20:36:34 +0800

    Modify AllTest Add MergeSort

--- test/AllTests.java
+++ test/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ BubbleSortTest.class, HeapSortTest.class,
-		InsertionSortTest.class })
+		InsertionSortTest.class,MergeSortTest.class })
 public class AllTests {
 
 }
